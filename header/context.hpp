4b6c8a07715d413afb8eeb5e9d467d436ef22889
chino <chino@test.com>
2023-08-16 17:40:29 +0800

    物理设备，逻辑设备，命令队列

--- header/context.hpp
+++ header/context.hpp
@@ -4,6 +4,7 @@
 #include "vulkan/vulkan.hpp"
 #include <memory>
 #include <iostream>
+#include<optional>
 
 namespace toy2d {
 	class Context final {
@@ -13,10 +14,25 @@ namespace toy2d {
 		static void Init();
 		static void Quit();
 		~Context();
+
 		vk::Instance instance;
+		vk::PhysicalDevice phyDevice;//物理设备
+		vk::Device device;//逻辑设备
+		struct  QueueFamilyIndices final
+		{
+			std::optional<uint32_t> graphicsQuene;
+		};
+		QueueFamilyIndices queueFamilyIndices;
+		vk::Queue graphicsQueue;
+
 	private:
 		Context();
 		static std::unique_ptr<Context> instance_;//智能指针，是这个类的一个实例，此时只是声明还未实例化
+		void createInstance();
+		void pickupPhysicalDevice();
+		void createDevice();
+		void queryQueueFamilyIndices();
+		void getQueues();
 	};
 
 }
--- src/context.cpp
+++ src/context.cpp
@@ -16,10 +16,23 @@ namespace toy2d {
 	}
 
 	Context::Context() {
+		createInstance();
+		pickupPhysicalDevice();
+		queryQueueFamilyIndices();
+		createDevice();
+		getQueues();
+	}
+
+	Context::~Context() {
+		device.destroy();
+		instance.destroy();
+	}
+	//创建instance
+	void Context::createInstance() {
 		vk::InstanceCreateInfo createInfo;//创建instance的info:结构体
 		vk::ApplicationInfo appInfo;
 		std::vector<const char*> layers = { "VK_LAYER_KHRONOS_validation" };
-	
+
 		appInfo.setApiVersion(VK_API_VERSION_1_3);
 		createInfo.setPApplicationInfo(&appInfo)
 			.setPEnabledLayerNames(layers);
@@ -27,7 +40,40 @@ namespace toy2d {
 		instance = vk::createInstance(createInfo);
 
 	}
-	Context::~Context() {
-		instance.destroy();
+	//挑选显卡
+	void Context::pickupPhysicalDevice(){
+		auto devices = instance.enumeratePhysicalDevices();
+		phyDevice = devices[0];
+		std::cout << phyDevice.getProperties().deviceName<<std::endl;
+	};
+	//创建逻辑设备
+	void Context::createDevice() {
+		vk::DeviceCreateInfo createInfo;
+		vk::DeviceQueueCreateInfo queneInfo;
+		float priorities = 1.0f;
+
+		queneInfo.setPQueuePriorities(&priorities)//优先级
+			.setQueueCount(1)//队列数
+			.setQueueFamilyIndex(queueFamilyIndices.graphicsQuene.value());//不同的设备不同，因此需要查询
+		createInfo.setQueueCreateInfos(queneInfo);//命令队列，用于从device到phydevice传指令
+
+		device = phyDevice.createDevice(createInfo);
+	
+	};
+	//请求到底哪个图形队列是命令队列
+	void Context::queryQueueFamilyIndices() {
+		auto properties = phyDevice.getQueueFamilyProperties();
+		for (int i = 0; i < properties.size();i++) {
+			auto property = properties[i];
+			if (property.queueFlags | vk::QueueFlagBits::eGraphics) {
+				queueFamilyIndices.graphicsQuene = i;
+				break;
+			}
+		}
+	}
+	//
+	void Context::getQueues() {
+		graphicsQueue = device.getQueue(queueFamilyIndices.graphicsQuene.value(),0);//第二个参数是拿哪一个队列，因为创建了一个所以是0
+
 	}
 }
